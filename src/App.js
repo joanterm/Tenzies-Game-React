1fa4294baa50b859d762e634a4d5bd638d6e7233
joanterm <jo_joanarg@yahoo.com>
2021-12-09 17:03:21 -0500

    Created an element that maps over random dice array and displays random numbers on the screen

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 import Die from './Components/Die';
 
 function App() {
 
   const allNewDice = () => {
-    const randomNumber = Math.floor(Math.random() * (7-1) + 1)
-    const randomArray = randomNumber.split('')
-    console.log(randomArray)
+    //CREATES AN ARRAY OF 10 RANDOM NUMBERS B/N 1-6
+    const randomArray = Array.from({length: 10}, () => {
+      return Math.floor(Math.random() * 6 + 1)
+    }) 
+    return randomArray
   }
 
+  const [die, setDie] = useState(allNewDice)
+
+  //WILL DISPLAY EACH VALUE FROM THE ARRAY IN EACH DIE COMPONENT (VALUE PASSED DOWN AS PROPS)
+  const dieElement = die.map((e) => {
+    return <Die value={e} />
+  })
+
+  // const allNewDice = () => {
+  //   const randomArray = []
+  //   for (let i=0; i<10; i++) {
+  //     randomArray.push(Math.floor(Math.random() * (7-1) + 1))
+  //   }
+  //   return randomArray
+  // }
+  // console.log(allNewDice())
 
   return (
     <div>
       <h1>Tenzies game</h1>
       <main>
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <button onClick={allNewDice}>TEST</button>
+        {dieElement}
       </main>
     </div>
   )
